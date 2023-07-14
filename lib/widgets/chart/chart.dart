2548f794e6664bad1ed2f6a587c8c4bf5189f130
Elizaveta Komarova <lisakomarova20000@gmail.com>
2023-07-14 13:01:41 +0300

    feat: add chart

---
+++ lib/widgets/chart/chart.dart
@@ -0,0 +1,99 @@
+import 'package:expense_app/models/expense.dart';
+import 'package:expense_app/models/expense_bucket.dart';
+import 'package:expense_app/widgets/chart/chart_bar.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Expense> expenses;
+
+  const Chart({Key? key, required this.expenses}) : super(key: key);
+
+  List<ExpenseBucket> get buckets {
+    return [
+      ExpenseBucket.forCategory(
+          allExpenses: expenses, category: ExpenseCategory.food),
+      ExpenseBucket.forCategory(
+          allExpenses: expenses, category: ExpenseCategory.leisure),
+      ExpenseBucket.forCategory(
+          allExpenses: expenses, category: ExpenseCategory.travel),
+      ExpenseBucket.forCategory(
+          allExpenses: expenses, category: ExpenseCategory.work),
+    ];
+  }
+
+  double get maxTotalExpense {
+    double maxTotalExpense = 0;
+
+    for (final bucket in buckets) {
+      if (bucket.totalExpenses > maxTotalExpense) {
+        maxTotalExpense = bucket.totalExpenses;
+      }
+    }
+
+    return maxTotalExpense;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 8,
+      ),
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.primary.withOpacity(0.3),
+            Theme.of(context).colorScheme.primary.withOpacity(0.0)
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                for (final bucket in buckets) // alternative to map()
+                  ChartBar(
+                    fill: bucket.totalExpenses == 0
+                        ? 0
+                        : bucket.totalExpenses / maxTotalExpense,
+                  )
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: buckets
+                .map(
+                  (bucket) => Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Icon(
+                        categoryItems[bucket.category],
+                        color: isDarkMode
+                            ? Theme.of(context).colorScheme.secondary
+                            : Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.7),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+
+  final double fill;
+
+  const ChartBar({Key? key, required this.fill}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: FractionallySizedBox(
+          heightFactor: fill,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(8)),
+              color: isDarkMode
+                  ? Theme.of(context).colorScheme.secondary
+                  : Theme.of(context).colorScheme.primary.withOpacity(0.65),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
